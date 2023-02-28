805b78d7f2d3f875738a0c7c7029209d67f60250
Kim Minjae <ffalswo2@naver.com>
2023-02-28 20:42:46 +0900

    이진탐색 - 가사검색 (미완)

---
+++ Minjae/Minjae/이분탐색/가사검색.swift
@@ -0,0 +1,18 @@
+//
+//  가사검색.swift
+//  Minjae
+//
+//  Created by 김민재 on 2023/02/17.
+//
+
+import Foundation
+
+
+func solution(_ words:[String], _ queries:[String]) -> [Int] {
+    return 0
+}
+
+_ = solution(
+    ["frodo", "front", "frost", "frozen", "frame", "kakao"],
+    ["fro??", "????o", "fr???", "fro???", "pro?"]
+)
